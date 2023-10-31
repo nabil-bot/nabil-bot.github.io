d0f513f3d7697ce3a1771c5e2de4a98765748aba
Nabil Mustofa <70287375+nabil-bot@users.noreply.github.com>
2023-11-01 02:57:08 +0600

    gn

--- Kontho/JavaScript/JavaScript.js
+++ Kontho/JavaScript/JavaScript.js
@@ -79,3 +79,25 @@ document.body.addEventListener('click', (event) => {
         }, 150);
     }, 10); // A small delay to allow resetting to take effect
 });
+
+
+
+
+
+
+(function ($) {
+  "use strict";
+
+  $(document).ready(function () {
+    // Initialize Tilt.js on your image
+    $(".card").tilt({
+      maxTilt: 18,
+      perspective: 1400,
+      easing: "cubic-bezier(.03,.98,.52,.99)",
+      speed: 1200,
+      glare: true,
+      maxGlare: 0.3,
+      scale: 1.04,
+    });
+  });
+})(jQuery);
