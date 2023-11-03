b8bc2722e3c5bcacf1e72e0c4be57076b814432e
Nabil Mustofa <70287375+nabil-bot@users.noreply.github.com>
2023-11-03 12:13:53 +0600

    fix

--- Kontho/JavaScript/JavaScript.js
+++ Kontho/JavaScript/JavaScript.js
@@ -101,3 +101,43 @@ document.body.addEventListener('click', (event) => {
     });
   });
 })(jQuery);
+
+
+
+
+
+
+
+
+
+// cursor animation
+
+// create instance of kinet with custom settings
+var kinet = new Kinet({
+  acceleration: 0.06,
+  friction: 0.20,
+  names: ["x", "y"],
+});
+
+// select circle element
+var circle = document.getElementById('circle');
+
+// set handler on kinet tick event
+kinet.on('tick', function(instances) {
+  circle.style.transform = `translate3d(${ (instances.x.current) }px, ${ (instances.y.current) }px, 0) rotateX(${ (instances.x.velocity/2) }deg) rotateY(${ (instances.y.velocity/2) }deg)`;
+});
+
+// call kinet animate method on mousemove
+document.addEventListener('mousemove', function (event) {
+  kinet.animate('x', event.clientX - window.innerWidth/2);
+  kinet.animate('y', event.clientY - window.innerHeight/2);
+});
+
+// log
+kinet.on('start', function() {
+  console.log('start');
+});
+
+kinet.on('end', function() {
+  console.log('end');
+});
