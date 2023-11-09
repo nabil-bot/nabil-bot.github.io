b24f03984d02a46de2d1ab47d141c5cf4af9ee91
Nabil Mustofa <70287375+nabil-bot@users.noreply.github.com>
2023-11-09 12:33:34 +0600

    2.48

--- Kontho/JavaScript/JavaScript.js
+++ Kontho/JavaScript/JavaScript.js
@@ -1,4 +1,17 @@
 
+$(document).ready(function() {
+  $("#variableValue").animate({
+    value: 2.48
+  }, {
+    duration: 4000,
+    easing: "linear",
+    step: function(now) {
+      $(this).text(now.toFixed(2));
+    }
+  });
+});
+
+
 //<!------- JavaScript for toggle mobile menu ------>
 var mainmenu = document.getElementById("mainmenu");
 function showMenu() {
@@ -106,38 +119,3 @@ document.body.addEventListener('click', (event) => {
 
 
 
-
-
-
-
-// cursor animation
-
-// create instance of kinet with custom settings
-var kinet = Kinet({
-  acceleration: 0.06,
-  friction: 0.20,
-  names: ["x", "y"],
-});
-
-// select circle element
-var circle = document.getElementById('circle');
-
-// set handler on kinet tick event
-kinet.on('tick', function(instances) {
-  circle.style.transform = `translate3d(${ (instances.x.current) }px, ${ (instances.y.current) }px, 0) rotateX(${ (instances.x.velocity/2) }deg) rotateY(${ (instances.y.velocity/2) }deg)`;
-});
-
-// call kinet animate method on mousemove
-document.addEventListener('mousemove', function (event) {
-  kinet.animate('x', event.clientX - window.innerWidth/2);
-  kinet.animate('y', event.clientY - window.innerHeight/2);
-});
-
-// log
-kinet.on('start', function() {
-  console.log('start');
-});
-
-kinet.on('end', function() {
-  console.log('end');
-});
